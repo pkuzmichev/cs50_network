b89d02bd15f62a7f96ea68753ca6c1a5faf9fd0f
Peter Kuzmichev <petya.kuzmichev@gmail.com>
2022-05-16 17:03:27 +0300

    init model for user following

--- network/models.py
+++ network/models.py
@@ -16,3 +16,10 @@ class Post(models.Model):
 
     def __str__(self):
         return self.text
+
+class Following(models.Model):
+    user_id = models.ForeignKey(User, related_name='following', on_delete=models.CASCADE)
+    following_user_id = models.ForeignKey(User, related_name='followers', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    # TODO: read: https://stackoverflow.com/questions/58794639/how-to-make-follower-following-system-with-django-model
