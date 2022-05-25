7d64c7198a7c7c58c1cede3bca67fda65bec0533
Peter Kuzmichev <petya.kuzmichev@gmail.com>
2022-05-25 20:17:59 +0300

    frontend edit posts

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -6,47 +6,37 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const buttonFollow = document.querySelector(".follow");
     const buttonUnfollow = document.querySelector(".unfollow");
     const editPostLink = document.querySelectorAll(".edit");
-    const post = document.querySelectorAll('.new-post');
+    // const post = document.querySelectorAll('.post');
 
     eventLike();
     editPost();
 
     // TODO: like/unlike fetch
-    buttonUnfollow.addEventListener("click", () => {
-        // fetch
-        console.log("click unfollow button");
-    })
+    // buttonUnfollow.addEventListener("click", () => {
+    //     // fetch
+    //     console.log("click unfollow button");
+    // })
 
+    // TODO:
+    // 1. close edit other edit open
+    // 2. backend
     function editPost() {
         for (let i = 0; i < editPostLink.length; i++) {
-            for (let p = 0; p < post.length; p++) {
-
-                editPostLink[i].addEventListener("click", () => {
-
-                    // 1. copy text post
-                    // 2. view textarea and button
-                    // TODO: add id post to new post
-                    post[p].innerHTML = `<div class="form-floating"><textarea class="form-control" id="floatingTextarea2" style="height: 10%; width: 50%;" maxlength="148" required name='new_post'></textarea></div><input class="btn btn-primary" type="submit" style="margin-top: 10px;" value="Post">`;
-                    // 3. paste text post
-                    // 4. save
-                    console.log('edit link click');
-                }
-
-                )
-            }
+            editPostLink[i].addEventListener("click", () => {
+                editPost = document.getElementById(editPostLink[i].id);
+                textPost = document.querySelector('.text-' + editPostLink[i].id);
+                editPost.innerHTML = `<div class="form-floating"><textarea class="form-control" id="floatingTextarea2" style="height: 10%; width: 50%;" maxlength="148" required name='new_post' autofocus>` + textPost.textContent + `</textarea></div><input class="btn btn-primary" type="submit" style="margin-top: 10px;" value="Save">`;
+            })
         }
     }
 
     function eventLike() {
         for (let i = 0; i < buttonLike.length; i++) {
             buttonLike[i].addEventListener("click", () => {
-                console.log('click');
                 if (buttonLike[i].classList.contains("liked")) {
-                    console.log('liked');
                     buttonLike[i].classList.remove("liked");
                     buttonLike[i].setAttribute('src', '/static/network/like.png');
                 } else {
-                    console.log('add liked');
                     buttonLike[i].setAttribute('src', '/static/network/unlike.png');
                     buttonLike[i].classList.add("liked");
                 }
