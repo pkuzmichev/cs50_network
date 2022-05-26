f2f87859c8586f483d70333d622395aa225d2697
Peter Kuzmichev <petya.kuzmichev@gmail.com>
2022-05-26 15:39:08 +0300

    fix frontend for edit post

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -6,8 +6,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const buttonFollow = document.querySelector(".follow");
     const buttonUnfollow = document.querySelector(".unfollow");
     const editPostLink = document.querySelectorAll(".edit");
-    let editFormOnScreen = false;
-    // const post = document.querySelectorAll('.post');
 
     eventLike();
     editPost();
@@ -19,30 +17,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
     // })
 
     // TODO:
-    // 1. close edit other edit open
-    // 2. backend
+    // 1. backend
+    // 2. security
     function editPost() {
         for (let i = 0; i < editPostLink.length; i++) {
             editPostLink[i].addEventListener("click", () => {
-                editFormOnScreen = true;
                 const post = document.getElementById(editPostLink[i].id);
-                console.log('post', post);
                 const editPost = document.getElementById(editPostLink[i].id);
-                if (editFormOnScreen) {
-                    console.log('if', post.outerHTML);
-                    // editPost.insertAdjacentElement('afterend', post);
-                    editPost.innerHTML = post.outerHTML;
-                    // const editPostNew = document.getElementById(editPostLink[i].id);
-                    // editPostNew.innerHTML = `<div class="form-floating form-edit-post"><textarea class="form-control" id="floatingTextarea2" style="height: 10%; width: 50%;" maxlength="148" required name='new_post' autofocus>` + textPost.textContent + `</textarea></div><input class="btn btn-primary" type="submit" style="margin-top: 10px;" value="Save">`;
-
-                } else {
-                    editFormOnScreen = true;
-                    textPost = document.querySelector('.text-' + editPostLink[i].id);
-                    editPost.innerHTML = `<div class="form-floating form-edit-post"><textarea class="form-control" id="floatingTextarea2" style="height: 10%; width: 50%;" maxlength="148" required name='new_post' autofocus>` + textPost.textContent + `</textarea></div><input class="btn btn-primary" type="submit" style="margin-top: 10px;" value="Save">`;
-                }
-                // const post = document.getElementById(editPostLink[i].id);
-                // console.log('post', post);
-                // const editPost = document.getElementById(editPostLink[i].id);
+                editPost.innerHTML = post.outerHTML;
+                textPost = document.querySelector('.text-' + editPostLink[i].id);
+                editPost.innerHTML = `<div class="form-floating form-edit-post"><textarea class="form-control" id="floatingTextarea2" style="height: 10%; width: 50%;" maxlength="148" required name='new_post' autofocus>` + textPost.textContent + `</textarea></div><input class="btn btn-primary" type="submit" style="margin-top: 10px;" value="Save">`;
             })
         }
     }
