de3fd61fab673659cc33d8bd3ad95fa62cab9f29
Peter Kuzmichev <petya.kuzmichev@gmail.com>
2022-05-26 11:37:55 +0300

    edit form frontend

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     const buttonFollow = document.querySelector(".follow");
     const buttonUnfollow = document.querySelector(".unfollow");
     const editPostLink = document.querySelectorAll(".edit");
+    let editFormOnScreen = false;
     // const post = document.querySelectorAll('.post');
 
     eventLike();
@@ -23,9 +24,25 @@ document.addEventListener("DOMContentLoaded", function (event) {
     function editPost() {
         for (let i = 0; i < editPostLink.length; i++) {
             editPostLink[i].addEventListener("click", () => {
-                editPost = document.getElementById(editPostLink[i].id);
-                textPost = document.querySelector('.text-' + editPostLink[i].id);
-                editPost.innerHTML = `<div class="form-floating"><textarea class="form-control" id="floatingTextarea2" style="height: 10%; width: 50%;" maxlength="148" required name='new_post' autofocus>` + textPost.textContent + `</textarea></div><input class="btn btn-primary" type="submit" style="margin-top: 10px;" value="Save">`;
+                editFormOnScreen = true;
+                const post = document.getElementById(editPostLink[i].id);
+                console.log('post', post);
+                const editPost = document.getElementById(editPostLink[i].id);
+                if (editFormOnScreen) {
+                    console.log('if', post.outerHTML);
+                    // editPost.insertAdjacentElement('afterend', post);
+                    editPost.innerHTML = post.outerHTML;
+                    // const editPostNew = document.getElementById(editPostLink[i].id);
+                    // editPostNew.innerHTML = `<div class="form-floating form-edit-post"><textarea class="form-control" id="floatingTextarea2" style="height: 10%; width: 50%;" maxlength="148" required name='new_post' autofocus>` + textPost.textContent + `</textarea></div><input class="btn btn-primary" type="submit" style="margin-top: 10px;" value="Save">`;
+
+                } else {
+                    editFormOnScreen = true;
+                    textPost = document.querySelector('.text-' + editPostLink[i].id);
+                    editPost.innerHTML = `<div class="form-floating form-edit-post"><textarea class="form-control" id="floatingTextarea2" style="height: 10%; width: 50%;" maxlength="148" required name='new_post' autofocus>` + textPost.textContent + `</textarea></div><input class="btn btn-primary" type="submit" style="margin-top: 10px;" value="Save">`;
+                }
+                // const post = document.getElementById(editPostLink[i].id);
+                // console.log('post', post);
+                // const editPost = document.getElementById(editPostLink[i].id);
             })
         }
     }
